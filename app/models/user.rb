f01cb477b8cc27f107e0603e2293bb18c0943627
Alexandria Irondina Keene <alexandria@Alexandrias-MacBook-Pro-2.local>
2020-06-15 13:53:12 -0500

    add user ids to articles migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
     VALID_EMAIL_REQEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
     before_save { self.email = email.downcase }
-
+    has_secure_password
 end
---
+++ db/migrate/20200615185116_add_user_is_to_articles.rb
@@ -0,0 +1,4 @@
+class AddUserIsToArticles < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
