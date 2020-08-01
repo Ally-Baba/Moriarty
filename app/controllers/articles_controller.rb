cb6ce4219a60570b365034d083a0bca3720eeb03
Alexandria Irondina Keene <alexandria@Alexandrias-MacBook-Pro-2.local>
2020-08-01 18:59:03 -0500

    define user privileges

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
     before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :logged_in_user, only: [:edit, :update, :delete]
     def set_article
         @article = Article.find(params[:id])
     end
@@ -26,7 +27,7 @@ class ArticlesController < ApplicationController
     
     def create 
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:success] = "Thanks for signing our guest book!"
             redirect_to article_path(@article)
@@ -58,6 +59,13 @@ class ArticlesController < ApplicationController
     private
         def article_params
             params.require(:article).permit(:title, :description)
+        end
+
+        def logged_in_user
+            unless current_user == @article.user
+              flash[:danger] = "You do not have authorization to edit this post" #if not logged in as the owner of the post
+              redirect_to articles_path
+            end
         end
 
 end
